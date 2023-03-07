fee75ba45fbd9d649d5005d6f20a6ab93bcce40b
Fran Nicolas <franciscojaviern1997@gmail.com>
2023-03-07 14:50:53 -0600

    lots of updates

--- TPCAnalyzer_module.cc
+++ TPCAnalyzer_module.cc
@@ -7,225 +7,10 @@
 // from cetlib version v3_10_00.
 ////////////////////////////////////////////////////////////////////////
 
-
-#include "art/Framework/Core/EDAnalyzer.h"
-#include "art/Framework/Core/ModuleMacros.h"
-#include "art/Framework/Principal/Event.h"
-#include "art/Framework/Principal/Handle.h"
-#include "art/Framework/Principal/Run.h"
-#include "art/Framework/Principal/SubRun.h"
-#include "art/Framework/Services/Registry/ServiceHandle.h"
-#include "art_root_io/TFileService.h"
-#include "art_root_io/TFileDirectory.h"
+#include "sbndcode/TPCAnalyzer/TPCAnalyzer_module.hh"
 
 
-#include "fhiclcpp/ParameterSet.h"
-
-#include "messagefacility/MessageLogger/MessageLogger.h"
-
-#include "art/Framework/Principal/Handle.h"
-#include "art/Framework/Services/Registry/ServiceHandle.h"
-
-#include "nusimdata/SimulationBase/MCTruth.h"
-#include "nusimdata/SimulationBase/MCNeutrino.h"
-#include "nusimdata/SimulationBase/MCParticle.h"
-
-#include "larcoreobj/SummaryData/POTSummary.h"
-#include "larcoreobj/SimpleTypesAndConstants/RawTypes.h"
-#include "larcoreobj/SimpleTypesAndConstants/readout_types.h"
-
-#include "larcore/Geometry/Geometry.h"
-
-#include "larcorealg/Geometry/GeometryData.h"
-#include "larcorealg/Geometry/GeometryCore.h"
-#include "larcorealg/Geometry/ChannelMapAlg.h"
-
-#include "lardata/RecoBaseProxy/ProxyBase/withCollectionProxy.h"
-#include "lardata/RecoBaseProxy/ProxyBase/withAssociated.h"
-#include "lardata/RecoBaseProxy/ProxyBase/withParallelData.h"
-#include "lardata/RecoBaseProxy/ProxyBase/withZeroOrOne.h"
-#include "lardata/RecoBaseProxy/ProxyBase/getCollection.h"
-#include "lardataobj/RecoBase/Vertex.h"
-#include "lardata/DetectorInfoServices/DetectorClocksService.h"
-#include "lardata/DetectorInfoServices/DetectorPropertiesService.h"
-
-#include "lardataalg/DetectorInfo/DetectorPropertiesData.h"
-#include "lardataalg/DetectorInfo/DetectorPropertiesStandard.h"
-
-#include "lardataobj/MCBase/MCTrack.h"
-#include "lardataobj/MCBase/MCShower.h"
-#include "lardataobj/MCBase/MCStep.h"
-#include "lardataobj/RawData/RawDigit.h"
-#include "lardataobj/RecoBase/Track.h"
-#include "lardataobj/RecoBase/Shower.h"
-#include "lardataobj/RecoBase/PFParticle.h"
-#include "lardataobj/RecoBase/Cluster.h"
-#include "lardataobj/RecoBase/Hit.h"
-#include "lardataobj/RecoBase/EndPoint2D.h"
-#include "lardataobj/Simulation/SimChannel.h"
-#include "lardataobj/AnalysisBase/Calorimetry.h"
-#include "lardataobj/AnalysisBase/ParticleID.h"
-#include "lardataobj/RawData/RawDigit.h"
-#include "lardataobj/RecoBase/Wire.h"
-#include "lardataobj/RawData/BeamInfo.h"
-#include "lardataobj/RawData/OpDetWaveform.h"
-#include "lardataobj/Simulation/SimPhotons.h"
-#include "lardataobj/RecoBase/MCSFitResult.h"
-
-#include "larreco/RecoAlg/TrackMomentumCalculator.h"
-#include "larreco/RecoAlg/TrajectoryMCSFitter.h"
-#include "larreco/RecoAlg/PMAlg/PmaTrack3D.h"
-#include "larreco/SpacePointSolver/TripletFinder.h"
-
-#include "larpandora/LArPandoraInterface/LArPandoraHelper.h"
-
-#include "larsim/MCCheater/BackTracker.h"
-#include "larsim/MCCheater/BackTrackerService.h"
-#include "larsim/MCCheater/ParticleInventoryService.h"
-#include "larsim/Utils/TruthMatchUtils.h"
-#include "larsim/MCCheater/ParticleInventory.h"
-#include "larsim/MCCheater/ParticleInventoryService.h"
-
-
-#include "canvas/Utilities/InputTag.h"
-#include "canvas/Persistency/Common/Ptr.h"
-#include "canvas/Persistency/Common/PtrVector.h"
-#include "canvas/Persistency/Common/FindMany.h"
-#include "canvas/Persistency/Common/FindManyP.h"
-#include "canvas/Persistency/Common/FindOneP.h"
-#include "canvas/Persistency/Common/Ptr.h"
-#include "canvas/Persistency/Common/PtrVector.h"
-
-#include "larevt/SpaceCharge/SpaceCharge.h"
-
-#include "sbndcode/RecoUtils/RecoUtils.h"
-
-#include "lardataobj/Simulation/SimEnergyDeposit.h"
-
-
-#include "TTree.h"
-#include "TFile.h"
-#include "TInterpreter.h"
-#include "TTimeStamp.h"
-
-#include <vector>
-#include <limits>
-#include <map>
-#include <sstream>
-#include <fstream>
-#include <iostream>
-
-#define fXFidCut 200
-#define fYFidCut 200
-#define fZFidCut1 5
-#define fZFidCut2 495
-
-namespace test {
-  class TPCAnalyzer
-;
-}
-
-
-class test::TPCAnalyzer : public art::EDAnalyzer {
-public:
-  explicit TPCAnalyzer
-(fhicl::ParameterSet const& p);
-  // The compiler-generated destructor is fine for non-base
-  // classes without bare pointers or other resource use.
-
-  // Plugins should not be copied or assigned.
-  TPCAnalyzer(TPCAnalyzer const&) = delete;
-  TPCAnalyzer(TPCAnalyzer&&) = delete;
-  TPCAnalyzer & operator=(TPCAnalyzer const&) = delete;
-  TPCAnalyzer & operator=(TPCAnalyzer &&) = delete;
-
-  // Required functions.
-  void analyze(art::Event const& e) override;
-
-  // Selected optional functions.
-  void beginJob() override;
-  void endJob() override;
-
-private:
-
-  // Declare member data here.
-  void resetVars();
-  int VertexToDriftTick(double vt, double vx);
-
-  std::string fMCTruthLabel;
-  std::string fSimEnergyDepositLabel;
-  std::string fSimEnergyDepositInstanceLabel;
-  std::string fRawDigitLabel;
-  std::string fRecobWireLabel;
-  std::string fHitLabel;
-  std::string fSpacePointLabel;
-  bool fSaveTruth;
-  bool fSaveSimED;
-  bool fSaveWaveforms;
-  bool fSaveWires;
-  bool fSaveHits;
-  bool fSaveSpacePoints;
-  bool fCreateTPCMap;
-  bool fApplyFiducialCut;
-
-  TTree* fTree;
-  int fEventID, fRunID, fSubRunID;
-
-  //True variables
-  std::vector<int> fTruePrimariesPDG;
-  std::vector<double> fTruePrimariesE;
-  double fTrueVx;
-  double fTrueVy;
-  double fTrueVz;
-  double fTrueVt;
-  int fTrueVU;
-  int fTrueVV;
-  int fTrueVC;
-  int fTrueVTimeTick;
-  double fTrueVEnergy;
-
-  //True SimEnergyDeposits
-  std::vector<double> fEnDepE;
-  std::vector<double> fEnDepX;
-  std::vector<double> fEnDepY;
-  std::vector<double> fEnDepZ;
-  std::vector<double> fEnDepT;
-
-  //Hit variables
-  std::vector<double> fHitsPeakTime;
-  std::vector<double> fHitsIntegral;
-  std::vector<double> fHitsChannel;
-
-  //Space Point Variables
-  std::vector<double> fSpacePointX;
-  std::vector<double> fSpacePointY;
-  std::vector<double> fSpacePointZ;
-  std::vector<double> fSpacePointIntegral;
-
-  //Waveforms
-  std::vector<std::vector<double>> fRawChannelADC;
-  std::vector<int> fRawChannelID;
-  std::vector<double> fRawChannelPedestal;
-
-  //Recob Wires
-  unsigned int fNROIs;
-  std::vector<std::vector<float>> fWireADC;
-  std::vector<unsigned int> fWireID;
-  std::vector<int> fWireStampTime;
-
-  int fNAnalyzedEvents;
-
-  const geo::GeometryCore* fGeom = art::ServiceHandle<geo::Geometry>()->provider();
-
-  unsigned int fNChannels;
-  unsigned int fReadoutWindow;
-  double fTriggerOffsetTPC;
-  double fTickPeriodTPC;
-  double fDriftVelocity;
-  double fWirePlanePosition;
-};
-
-
+// Constructor
 test::TPCAnalyzer::TPCAnalyzer(fhicl::ParameterSet const& p)
   : EDAnalyzer{p},
   fMCTruthLabel( p.get<std::string>("MCTruthLabel", "generator") ),
@@ -235,14 +20,17 @@ test::TPCAnalyzer::TPCAnalyzer(fhicl::ParameterSet const& p)
   fRecobWireLabel( p.get<std::string>("RecobWireLabel", "caldata") ),
   fHitLabel( p.get<std::string>("HitLabel", "gaushit") ),
   fSpacePointLabel( p.get<std::string>("SpacePointLabel", "pandora") ),
+  fVertexLabel( p.get<std::string>("VertexLabel", "pandora") ),
   fSaveTruth( p.get<bool>("SaveTruth", "true") ),
   fSaveSimED( p.get<bool>("SaveSimED", "true") ),
   fSaveWaveforms( p.get<bool>("SaveWaveforms", "false") ),
   fSaveWires( p.get<bool>("SaveWires", "false") ),
   fSaveHits( p.get<bool>("SaveHits", "true") ),
   fSaveSpacePoints( p.get<bool>("SaveSpacePoints", "false") ),
+  fSaveVertex( p.get<bool>("SaveVertex", "false") ),
   fCreateTPCMap( p.get<bool>("CreateTPCMap", "false") ),
   fApplyFiducialCut( p.get<bool>("ApplyFiducialCut", "true") ),
+  fApplyVertexSCE( p.get<bool>("ApplyVertexSCE", "true") ),
   fNChannels(fGeom->Nchannels())
   // More initializers here.
 {
@@ -266,12 +54,12 @@ test::TPCAnalyzer::TPCAnalyzer(fhicl::ParameterSet const& p)
 }
 
 
+// Main function
 void test::TPCAnalyzer::analyze(art::Event const& e)
 {
   // Implementation of required member function here.
+  auto fSCE = lar::providerFrom<spacecharge::SpaceChargeService>();
   std::cout<<"Running TPCAnalyzer---run="<< e.id().run()<<" --subrun="<< e.id().subRun()<<" --event="<<e.id().event()<<"\n";
-  //auto const fClockData = art::ServiceHandle<detinfo::DetectorClocksService const>()->DataFor(e);
-  //auto const fDetProp = art::ServiceHandle<detinfo::DetectorPropertiesService const>()->DataFor(e, fClockData);
 
   //............................Event General Info
   fNAnalyzedEvents++;
@@ -305,6 +93,17 @@ void test::TPCAnalyzer::analyze(art::Event const& e)
           fTrueVt=TruePart.T();
           fTrueVEnergy=TruePart.E();
 
+          if(fApplyVertexSCE){
+
+            auto vOffset = fSCE->GetPosOffsets({fTrueVx, fTrueVy, fTrueVz});
+            fTrueVx -= vOffset.X();
+            fTrueVy += vOffset.Y();
+            fTrueVz += vOffset.Z();
+
+            std::cout<<"Applying SCE: "<<vOffset.X()<<" "<<vOffset.Y()<<" "<<vOffset.Z()<<std::endl;
+            std::cout<<fSCE->EnableSimSpatialSCE()<<" "<<fSCE->EnableSimEfieldSCE()<<" "<<fSCE->EnableCorrSCE()<<" "<<fSCE->EnableCalSpatialSCE()<<std::endl;
+          }
+          
           if(fApplyFiducialCut && std::abs(fTrueVx)<fXFidCut && std::abs(fTrueVy)<fYFidCut && fTrueVz>fZFidCut1 && fTrueVz<fZFidCut2){
             const double p[3]={fTrueVx, fTrueVy, fTrueVz};
             std::cout<<"HasTPC: "<<fGeom->HasTPC(fGeom->FindTPCAtPosition(p))<<" "<<fGeom->FindTPCAtPosition(p).TPC<<std::endl;
@@ -416,6 +215,8 @@ void test::TPCAnalyzer::analyze(art::Event const& e)
         fHitsPeakTime.push_back(hit->PeakTime());
         fHitsIntegral.push_back(hit->Integral());
         fHitsChannel.push_back(hit->Channel());
+        fHitsChi2.push_back(hit->GoodnessOfFit());
+        fHitsNDF.push_back(hit->DegreesOfFreedom());
       }
     }
 
@@ -444,9 +245,89 @@ void test::TPCAnalyzer::analyze(art::Event const& e)
     }
   }
 
+  //............................Read Reco Vertex
+  if(fSaveVertex){
+    art::Handle< std::vector<recob::PFParticle> > eventPFParticle;
+    std::vector< art::Ptr<recob::PFParticle> > pfparticleVect;
+    if(e.getByLabel(fVertexLabel, eventPFParticle))
+      art::fill_ptr_vector(pfparticleVect, eventPFParticle);
+    std::cout<<" --- Saving Reconstructed Vertex\n";
+
+    size_t neutrinoID = fDefaulNeutrinoID;
+    std::cout<<"   *** PFParticle size:"<<pfparticleVect.size()<<std::endl;
+    for(const art::Ptr<recob::PFParticle> &pfp : pfparticleVect){
+      std::cout<<"PFParticlePDG:"<<pfp->PdgCode()<<" Primary="<<pfp->IsPrimary()<<std::endl;
+      if( !( pfp->IsPrimary() && ( std::abs(pfp->PdgCode())==12 || std::abs(pfp->PdgCode())==14 ) ) ) continue;
+      neutrinoID = pfp->Self();
+    }
+    std::cout<<"    ** NeutrinoID:"<<neutrinoID<<"\n\n";
+
+    if(neutrinoID != fDefaulNeutrinoID){
+      //Get Vertex Association
+      art::FindManyP<recob::Vertex> vertexAssoc (pfparticleVect, e, fVertexLabel);
+
+      //PFParticle loop
+      for(const art::Ptr<recob::PFParticle> &pfp : pfparticleVect){
+
+        //Get PFParticle Vertex
+        std::vector< art::Ptr<recob::Vertex> > vertexVec = vertexAssoc.at(pfp.key());
+
+        /*//PFParticle's track loop
+        if(pfpTracks.empty()) {
+          continue;
+        }*/
+
+        //PFParticle's vertex loop
+        std::cout<<"     PFParticle: "<<pfp->Self()<<std::endl;
+        for(const art::Ptr<recob::Vertex> &ver : vertexVec){
+          double xyz_vertex[3];
+          ver->XYZ(xyz_vertex);
+          std::vector<double> xyz_vec(std::begin(xyz_vertex), std::end(xyz_vertex));
+          //fpfpVertexPosition.push_back( xyz_vec );
+          double chi2=ver->chi2(), chi2ndof=ver->chi2PerNdof();
+          std::cout<<"  --VERTEX  ID="<<ver->ID()<<"  x,y,z="<<xyz_vertex[0]<<","<<xyz_vertex[1]<<","<<xyz_vertex[2];
+          std::cout<<" Chi2="<<chi2<<" Chi2/DoF="<<chi2ndof<<" Status:"<<ver->status()<<",\n";
+        }
+
+        //Fill neutrino vertex
+        if(pfp->Self()==neutrinoID){
+          double xyz_vertex[3];
+          vertexVec[0]->XYZ(xyz_vertex);
+          std::cout<<"  Filling neutrino vertex...\n";
+
+          fRecoVx= xyz_vertex[0];
+          fRecoVy= xyz_vertex[1];
+          fRecoVz= xyz_vertex[2];
+
+          if(fApplyFiducialCut && std::abs(fRecoVx)<fXFidCut && std::abs(fRecoVy)<fYFidCut && fRecoVz>fZFidCut1 && fRecoVz<fZFidCut2){
+            //const double p[3]={fVx, fTrueVy, fTrueVz};
+            std::cout<<"HasTPC: "<<fGeom->HasTPC(fGeom->FindTPCAtPosition(xyz_vertex))<<" "
+              <<fGeom->FindTPCAtPosition(xyz_vertex).TPC<<std::endl;
+
+            if( fGeom->HasTPC(fGeom->FindTPCAtPosition(xyz_vertex)) ){
+              unsigned int tpcID=fGeom->FindTPCAtPosition(xyz_vertex).TPC;
+              fRecoVU=fGeom->NearestChannel(xyz_vertex, 0, tpcID, 0);
+              fRecoVV=fGeom->NearestChannel(xyz_vertex, 1, tpcID, 0);
+              fRecoVC=fGeom->NearestChannel(xyz_vertex, 2, tpcID, 0);
+              fRecoVTimeTick=VertexToDriftTick(fTrueVt, fRecoVx);
+            }
+          }
+          else{
+            fRecoVU=-1;
+            fRecoVV=-1;
+            fRecoVC=-1;
+            fRecoVTimeTick=-1;
+          }
+        }
+      }
+    }
+
+  }
+
 
   fTree->Fill();
 }
+
 
 int test::TPCAnalyzer::VertexToDriftTick(double vt, double vx){
   return int( ( vt/1000 + ( fWirePlanePosition-std::abs(vx) )/fDriftVelocity - fTriggerOffsetTPC)/fTickPeriodTPC );
@@ -499,6 +380,8 @@ void test::TPCAnalyzer::resetVars()
     fHitsIntegral.clear();
     fHitsPeakTime.clear();
     fHitsChannel.clear();
+    fHitsChi2.clear();
+    fHitsNDF.clear();
   }
 
   if(fSaveSpacePoints){
@@ -508,102 +391,14 @@ void test::TPCAnalyzer::resetVars()
     fSpacePointIntegral.clear();
   }
 
-}
-
-
-
-void test::TPCAnalyzer::beginJob()
-{
-  // Implementation of optional member function here.
-  art::ServiceHandle<art::TFileService> tfs;
-
-  fTree=tfs->make<TTree>("AnaTPCTree", "Analysis Output Tree");
-  fTree->Branch("RunID", &fRunID, "RunID/I");
-  fTree->Branch("SubRunID", &fSubRunID, "SubRunID/I");
-  fTree->Branch("EventID", &fEventID, "EventID/I");
-
-  if(fSaveTruth){
-    fTree->Branch("TruePrimariesPDG", &fTruePrimariesPDG);
-    fTree->Branch("TruePrimariesE", &fTruePrimariesE);
-    fTree->Branch("TrueVx", &fTrueVx, "TrueVx/D");
-    fTree->Branch("TrueVy", &fTrueVy, "TrueVy/D");
-    fTree->Branch("TrueVz", &fTrueVz, "TrueVz/D");
-    fTree->Branch("TrueVt", &fTrueVt, "TrueVt/D");
-    fTree->Branch("TrueVU", &fTrueVU, "TrueVU/I");
-    fTree->Branch("TrueVV", &fTrueVV, "TrueVV/I");
-    fTree->Branch("TrueVC", &fTrueVC, "TrueVC/I");
-    fTree->Branch("TrueVTimeTick", &fTrueVTimeTick, "TrueVC/I");
-    fTree->Branch("TrueVEnergy", &fTrueVEnergy, "TrueVEnergy/D");
-  }
-
-  if(fSaveSimED){
-    fTree->Branch("EnDepE", &fEnDepE);
-    fTree->Branch("EnDepX", &fEnDepX);
-    fTree->Branch("EnDepY", &fEnDepY);
-    fTree->Branch("EnDepZ", &fEnDepZ);
-    fTree->Branch("EnDepT", &fEnDepT);
-  }
-
-  if(fSaveWaveforms){
-    fTree->Branch("RawChannelADC", &fRawChannelADC);
-    //fTree->Branch("RawChannelID", &fRawChannelID);
-    //fTree->Branch("RawChannelPedestal", &fRawChannelPedestal);
-  }
-
-  if(fSaveWires){
-    fTree->Branch("NROIs", &fNROIs);
-    fTree->Branch("WireID", &fWireID);
-    fTree->Branch("WireStampTime", &fWireStampTime);
-    fTree->Branch("WireADC", &fWireADC);
-    //fTree->Branch("RawChannelID", &fRawChannelID);
-    //fTree->Branch("RawChannelPedestal", &fRawChannelPedestal);
-  }
-
-  if(fSaveWires){
-    fTree->Branch("RawChannelADC", &fRawChannelADC);
-    //fTree->Branch("RawChannelID", &fRawChannelID);
-    //fTree->Branch("RawChannelPedestal", &fRawChannelPedestal);
-  }
-
-  if(fSaveHits){
-    fTree->Branch("HitsIntegral", &fHitsIntegral);
-    fTree->Branch("HitsPeakTime", &fHitsPeakTime);
-    fTree->Branch("HitsChannel", &fHitsChannel);
-  }
-
-  if(fSaveSpacePoints){
-    fTree->Branch("SpacePointX", &fSpacePointX);
-    fTree->Branch("SpacePointY", &fSpacePointY);
-    fTree->Branch("SpacePointZ", &fSpacePointZ);
-    fTree->Branch("SpacePointIntegral", &fSpacePointIntegral);
-  }
-
-  fNAnalyzedEvents=0;
-}
-
-void test::TPCAnalyzer::endJob(){
-
-  if(fCreateTPCMap){
-    std::ofstream fileout("TPCMapping.txt");
-    std::ofstream fileoutXYZ("TPCMappingXYZ.txt");
-    if(fileout.is_open()){
-        double xyz_start[3], xyz_end[3];;
-        for(unsigned int ch=0; ch<fGeom->Nchannels(); ch++){
-          std::vector<geo::WireID> wireV = fGeom->ChannelToWire(ch);
-          for(size_t w=0; w<wireV.size(); w++){
-            fGeom->WireEndPoints(wireV[w], xyz_start, xyz_end);
-            fileout<<ch<<" "<<wireV[w].Plane<<" "<<wireV[w].TPC<<std::endl;
-            fileoutXYZ<<ch<<" "<<wireV[w].Plane<<" "<<wireV[w].TPC<<" ";
-            fileoutXYZ<<xyz_start[0]<<" "<<xyz_start[1]<<" "<<xyz_start[2]<<" ";
-            fileoutXYZ<<xyz_end[0]<<" "<<xyz_end[1]<<" "<<xyz_end[2]<<std::endl;
-            std::cout<<ch<<":"<<w<<" ID="<<wireV[w].Wire<<" Plane="<<wireV[w].Plane<<" TPC="<<wireV[w].TPC<<std::endl;
-          }
-        }
-    }
-    fileout.close();
-    fileoutXYZ.close();
+  if(fSaveVertex){
+    fRecoVx=-1e3;
+    fRecoVy=-1e3;
+    fRecoVz=-1e3;
+    fRecoVU=-1;
+    fRecoVV=-1;
+    fRecoVC=-1;
+    fRecoVTimeTick=-1;
   }
 
 }
-
-DEFINE_ART_MODULE(test::TPCAnalyzer)
---
+++ TPCAnalyzer_module.hh
@@ -0,0 +1,304 @@
+////////////////////////////////////////////////////////////////////////
+// Class:       TPCAnalyzer
+// Plugin Type: analyzer (art v3_05_01)
+// File:        TPCAnalyzer_module.cc
+//
+// Generated at Mon Mar 15 04:43:39 2021 by Marina Bravo using cetskelgen
+// from cetlib version v3_10_00.
+////////////////////////////////////////////////////////////////////////
+
+#include "art/Framework/Core/EDAnalyzer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Principal/Run.h"
+#include "art/Framework/Principal/SubRun.h"
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "larcore/CoreUtils/ServiceUtil.h"
+#include "art_root_io/TFileService.h"
+#include "art_root_io/TFileDirectory.h"
+
+#include "canvas/Utilities/InputTag.h"
+#include "canvas/Persistency/Common/Ptr.h"
+#include "canvas/Persistency/Common/PtrVector.h"
+#include "canvas/Persistency/Common/FindMany.h"
+#include "canvas/Persistency/Common/FindManyP.h"
+#include "canvas/Persistency/Common/FindOneP.h"
+#include "canvas/Persistency/Common/Ptr.h"
+#include "canvas/Persistency/Common/PtrVector.h"
+
+#include "fhiclcpp/ParameterSet.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+
+#include "lardata/DetectorInfoServices/DetectorPropertiesService.h"
+#include "larcore/Geometry/Geometry.h"
+#include "larevt/SpaceCharge/SpaceCharge.h"
+#include "larevt/SpaceChargeServices/SpaceChargeService.h"
+
+#include "nusimdata/SimulationBase/MCTruth.h"
+#include "nusimdata/SimulationBase/MCNeutrino.h"
+#include "nusimdata/SimulationBase/MCParticle.h"
+#include "lardataobj/Simulation/SimEnergyDeposit.h"
+#include "lardataobj/RawData/RawDigit.h"
+#include "lardataobj/RecoBase/Track.h"
+#include "lardataobj/RecoBase/Shower.h"
+#include "lardataobj/RecoBase/PFParticle.h"
+#include "lardataobj/RecoBase/SpacePoint.h"
+#include "lardataobj/RecoBase/Cluster.h"
+#include "lardataobj/RecoBase/Hit.h"
+#include "lardataobj/RawData/RawDigit.h"
+#include "lardataobj/RecoBase/Wire.h"
+#include "lardataobj/RecoBase/Vertex.h"
+
+
+#include "TTree.h"
+#include "TFile.h"
+#include "TInterpreter.h"
+#include "TTimeStamp.h"
+
+#include <vector>
+#include <limits>
+#include <map>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+
+#define fXFidCut 200
+#define fYFidCut 200
+#define fZFidCut1 5
+#define fZFidCut2 495
+
+#define fDefaulNeutrinoID 99999
+
+namespace test {
+  class TPCAnalyzer
+;
+}
+
+
+class test::TPCAnalyzer : public art::EDAnalyzer {
+public:
+  explicit TPCAnalyzer
+(fhicl::ParameterSet const& p);
+  // The compiler-generated destructor is fine for non-base
+  // classes without bare pointers or other resource use.
+
+  // Plugins should not be copied or assigned.
+  TPCAnalyzer(TPCAnalyzer const&) = delete;
+  TPCAnalyzer(TPCAnalyzer&&) = delete;
+  TPCAnalyzer & operator=(TPCAnalyzer const&) = delete;
+  TPCAnalyzer & operator=(TPCAnalyzer &&) = delete;
+
+  // Required functions.
+  void analyze(art::Event const& e) override;
+
+  // Selected optional functions.
+  void beginJob() override;
+  void endJob() override;
+
+private:
+
+  // Declare member data here.
+  void resetVars();
+  int VertexToDriftTick(double vt, double vx);
+
+  std::string fMCTruthLabel;
+  std::string fSimEnergyDepositLabel;
+  std::string fSimEnergyDepositInstanceLabel;
+  std::string fRawDigitLabel;
+  std::string fRecobWireLabel;
+  std::string fHitLabel;
+  std::string fSpacePointLabel;
+  std::string fVertexLabel;
+  bool fSaveTruth;
+  bool fSaveSimED;
+  bool fSaveWaveforms;
+  bool fSaveWires;
+  bool fSaveHits;
+  bool fSaveSpacePoints;
+  bool fSaveVertex;
+  bool fCreateTPCMap;
+  bool fApplyFiducialCut;
+  bool fApplyVertexSCE;
+
+  TTree* fTree;
+  int fEventID, fRunID, fSubRunID;
+
+  //True variables
+  std::vector<int> fTruePrimariesPDG;
+  std::vector<double> fTruePrimariesE;
+  double fTrueVx;
+  double fTrueVy;
+  double fTrueVz;
+  double fTrueVt;
+  int fTrueVU;
+  int fTrueVV;
+  int fTrueVC;
+  int fTrueVTimeTick;
+  double fTrueVEnergy;
+
+  //True SimEnergyDeposits
+  std::vector<double> fEnDepE;
+  std::vector<double> fEnDepX;
+  std::vector<double> fEnDepY;
+  std::vector<double> fEnDepZ;
+  std::vector<double> fEnDepT;
+
+  //Hit variables
+  std::vector<double> fHitsPeakTime;
+  std::vector<double> fHitsIntegral;
+  std::vector<double> fHitsChannel;
+  std::vector<double> fHitsChi2;
+  std::vector<double> fHitsNDF;
+
+  //Space Point Variables
+  std::vector<double> fSpacePointX;
+  std::vector<double> fSpacePointY;
+  std::vector<double> fSpacePointZ;
+  std::vector<double> fSpacePointIntegral;
+
+  //Waveforms
+  std::vector<std::vector<double>> fRawChannelADC;
+  std::vector<int> fRawChannelID;
+  std::vector<double> fRawChannelPedestal;
+
+  //Recob Wires
+  unsigned int fNROIs;
+  std::vector<std::vector<float>> fWireADC;
+  std::vector<unsigned int> fWireID;
+  std::vector<int> fWireStampTime;
+
+  //Reconstructed vertex
+  double fRecoVx;
+  double fRecoVy;
+  double fRecoVz;
+  int fRecoVU;
+  int fRecoVV;
+  int fRecoVC;
+  int fRecoVTimeTick;
+
+  int fNAnalyzedEvents;
+
+  const geo::GeometryCore* fGeom = art::ServiceHandle<geo::Geometry>()->provider();
+
+  unsigned int fNChannels;
+  unsigned int fReadoutWindow;
+  double fTriggerOffsetTPC;
+  double fTickPeriodTPC;
+  double fDriftVelocity;
+  double fWirePlanePosition;
+};
+
+
+
+
+
+void test::TPCAnalyzer::beginJob()
+{
+  // Implementation of optional member function here.
+  art::ServiceHandle<art::TFileService> tfs;
+
+  fTree=tfs->make<TTree>("AnaTPCTree", "Analysis Output Tree");
+  fTree->Branch("RunID", &fRunID, "RunID/I");
+  fTree->Branch("SubRunID", &fSubRunID, "SubRunID/I");
+  fTree->Branch("EventID", &fEventID, "EventID/I");
+
+  if(fSaveTruth){
+    fTree->Branch("TruePrimariesPDG", &fTruePrimariesPDG);
+    fTree->Branch("TruePrimariesE", &fTruePrimariesE);
+    fTree->Branch("TrueVx", &fTrueVx, "TrueVx/D");
+    fTree->Branch("TrueVy", &fTrueVy, "TrueVy/D");
+    fTree->Branch("TrueVz", &fTrueVz, "TrueVz/D");
+    fTree->Branch("TrueVt", &fTrueVt, "TrueVt/D");
+    fTree->Branch("TrueVU", &fTrueVU, "TrueVU/I");
+    fTree->Branch("TrueVV", &fTrueVV, "TrueVV/I");
+    fTree->Branch("TrueVC", &fTrueVC, "TrueVC/I");
+    fTree->Branch("TrueVTimeTick", &fTrueVTimeTick, "TrueVC/I");
+    fTree->Branch("TrueVEnergy", &fTrueVEnergy, "TrueVEnergy/D");
+  }
+
+  if(fSaveSimED){
+    fTree->Branch("EnDepE", &fEnDepE);
+    fTree->Branch("EnDepX", &fEnDepX);
+    fTree->Branch("EnDepY", &fEnDepY);
+    fTree->Branch("EnDepZ", &fEnDepZ);
+    fTree->Branch("EnDepT", &fEnDepT);
+  }
+
+  if(fSaveWaveforms){
+    fTree->Branch("RawChannelADC", &fRawChannelADC);
+    //fTree->Branch("RawChannelID", &fRawChannelID);
+    //fTree->Branch("RawChannelPedestal", &fRawChannelPedestal);
+  }
+
+  if(fSaveWires){
+    fTree->Branch("NROIs", &fNROIs);
+    fTree->Branch("WireID", &fWireID);
+    fTree->Branch("WireStampTime", &fWireStampTime);
+    fTree->Branch("WireADC", &fWireADC);
+    //fTree->Branch("RawChannelID", &fRawChannelID);
+    //fTree->Branch("RawChannelPedestal", &fRawChannelPedestal);
+  }
+
+  if(fSaveWires){
+    fTree->Branch("RawChannelADC", &fRawChannelADC);
+    //fTree->Branch("RawChannelID", &fRawChannelID);
+    //fTree->Branch("RawChannelPedestal", &fRawChannelPedestal);
+  }
+
+  if(fSaveHits){
+    fTree->Branch("HitsIntegral", &fHitsIntegral);
+    fTree->Branch("HitsPeakTime", &fHitsPeakTime);
+    fTree->Branch("HitsChannel", &fHitsChannel);
+    fTree->Branch("HitsChi2", &fHitsChi2);
+    fTree->Branch("HitsNDF", &fHitsNDF);
+  }
+
+  if(fSaveSpacePoints){
+    fTree->Branch("SpacePointX", &fSpacePointX);
+    fTree->Branch("SpacePointY", &fSpacePointY);
+    fTree->Branch("SpacePointZ", &fSpacePointZ);
+    fTree->Branch("SpacePointIntegral", &fSpacePointIntegral);
+  }
+
+  if(fSaveVertex){
+    fTree->Branch("RecoVx", &fRecoVx, "RecoVx/D");
+    fTree->Branch("RecoVy", &fRecoVy, "RecoVy/D");
+    fTree->Branch("RecoVz", &fRecoVz, "RecoVz/D");
+    fTree->Branch("RecoVU", &fRecoVU, "RecoVU/I");
+    fTree->Branch("RecoVV", &fRecoVV, "RecoVV/I");
+    fTree->Branch("RecoVC", &fRecoVC, "RecoVC/I");
+    fTree->Branch("RecoVTimeTick", &fRecoVTimeTick, "RecoVC/I");
+  }
+
+  fNAnalyzedEvents=0;
+}
+
+void test::TPCAnalyzer::endJob(){
+
+  if(fCreateTPCMap){
+    std::ofstream fileout("TPCMapping.txt");
+    std::ofstream fileoutXYZ("TPCMappingXYZ.txt");
+    if(fileout.is_open()){
+        double xyz_start[3], xyz_end[3];;
+        for(unsigned int ch=0; ch<fGeom->Nchannels(); ch++){
+          std::vector<geo::WireID> wireV = fGeom->ChannelToWire(ch);
+          for(size_t w=0; w<wireV.size(); w++){
+            fGeom->WireEndPoints(wireV[w], xyz_start, xyz_end);
+            fileout<<ch<<" "<<wireV[w].Plane<<" "<<wireV[w].TPC<<std::endl;
+            fileoutXYZ<<ch<<" "<<wireV[w].Plane<<" "<<wireV[w].TPC<<" ";
+            fileoutXYZ<<xyz_start[0]<<" "<<xyz_start[1]<<" "<<xyz_start[2]<<" ";
+            fileoutXYZ<<xyz_end[0]<<" "<<xyz_end[1]<<" "<<xyz_end[2]<<std::endl;
+            std::cout<<ch<<":"<<w<<" ID="<<wireV[w].Wire<<" Plane="<<wireV[w].Plane<<" TPC="<<wireV[w].TPC<<std::endl;
+          }
+        }
+    }
+    fileout.close();
+    fileoutXYZ.close();
+  }
+
+}
+
+DEFINE_ART_MODULE(test::TPCAnalyzer)
